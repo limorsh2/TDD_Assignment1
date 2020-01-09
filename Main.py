6d0a797acf5b9c81bfc07e0c88c7eeb53a52a57d
mor220 <hasid123@gmail.com>
2020-01-09 11:56:42 +0200

    added Main file

---
+++ Main.py
@@ -0,0 +1,26 @@
+
+
+choose = 0
+while choose != 1 or choose !=2:
+    choose = input("press 1 for IBM function or 2 for Bubble sort")
+
+if choose == 1:
+    height = input("Enter height: ")
+    weight = input("Enter weight: ")
+    print(IBM(height, weight))
+else:
+    lst = []
+    n = int(input("Enter number of elements : "))
+    for i in range(0, n):
+        ele = int(input())
+        lst.append(ele)
+    print(BubbleSort(ist))
+    
+
+
+
+
+
+
+
+
